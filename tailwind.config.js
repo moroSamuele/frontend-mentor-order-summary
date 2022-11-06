0c58129b8115fa46291b6ef70b76b7da38f98494
Samuele Moro <moro96.samuele@icloud.com>
2022-11-06 09:52:25 +0100

    Order Summary Challenge

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        paleBlue: "hsl(225, 100%, 94%)",
+        brightBlue: "hsl(245, 75%, 52%)",
+        hoverBrigthBlue: "hsl(244, 78%, 68%)",
+  
+        veryPaleBlue: "hsl(225, 100%, 98%)",
+        desaturatedBlue: "hsl(224, 23%, 55%)",
+        darkBlue: "hsl(223, 47%, 23%)",
+      },
+      boxShadow: {
+        '3xl': '0 20px 25px -20px rgba(40, 27, 187, 0.8)',
+      }
+    },
+  },
+  plugins: [],
+}
